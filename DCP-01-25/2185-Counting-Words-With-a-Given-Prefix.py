e9d62e039ca81b97edefd961a75431f9e9d357cb
MohamadCh1 <123898041+MohamadCh1@users.noreply.github.com>
2025-01-10 00:20:56 +0200

    Delete DCP-01-25 directory

--- DCP-01-25/2185-Counting-Words-With-a-Given-Prefix.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def prefixCount(self, words: List[str], pref: str) -> int:
-        x=0
-        for word in words:
-            if(word[:len(pref)]==pref):
-                x=x+1
-        return x
