d993df4caf49a56de5a7ce717e10679a9b1a2a08
MohamadCh1 <123898041+MohamadCh1@users.noreply.github.com>
2026-05-25 20:04:29 +0300

    Time: 0 ms (100%), Space: 19.5 MB (13.4%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -7,28 +7,15 @@ class Solution:
         out = ""
         for i in range(0,len(s)):
             if len(out)==0:
-                if s[i]=="-":
-                    out = out + "-"
+                if s[i] in ["+","-"]:
+                    out = out + s[i]
                     continue
-                if s[i]=="+":
-                    out = out + "+"
-                    continue
-                if s[i]==" ":
+                elif s[i]==" ":
                     continue
             if s[i].isdigit():
                 out = out + s[i]
             else: 
-                if out=="-" or out=="+":
-                    return 0
-                else:
-                    if out == "":
-                        return 0
-                    out = int(out)
-                    if out < -2147483648:
-                        return -2147483648
-                    if out > 2147483647:
-                        return 2147483647
-                    return out 
+                break 
         if out == "" or out=="-" or out=="+":
             return 0
         out = int(out)
